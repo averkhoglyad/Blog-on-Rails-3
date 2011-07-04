5b699eac26a96edeeef79a62eb35a0cdb6e210d8
Alexander Verkhoglyad <averkhoglyad@ideas-implemented.com>
2011-07-04 16:44:16 +0300

    create empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'fcbfc97021f1acace23129a129b57321210c5cd579800a614b2a598b0106083b742c10a846f70611186886acdcf820f5c59c020e3bd1302c35ced41ff125fd1f'
